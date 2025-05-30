06e48aa9bf39687836746d9d84fbc92a2a9d204f
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-05-30 13:30:14 +0200

    managed api fetch sucessfully

--- fetch.js
+++ fetch.js
@@ -1,3 +1,15 @@
-'use strict'
-mainFetch = document.getElementById('mainFetch') 
-console.log(mainFetch)
+'use strict'; 
+const mainFetch = document.getElementById('mainFetch');
+async function fetchData(params) {
+    fetch(params)
+    .then((response) => response.json())
+    .then((result) => result.map(function(elem) {
+        
+        const h2 = document.createElement("h2");
+        h2.textContent = `${elem.name}`;
+        mainFetch.appendChild(h2)
+    }))
+    .catch((error) => console.log(error));
+}
+
+fetchData('https://emojihub.yurace.pro/api/all');
